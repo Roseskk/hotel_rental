845f80373c7134f7f0ec737726bf6bc51ed25bbd
Roseskk <oleg419583@yandex.ru>
2023-07-02 14:46:32 +0300

    Home page

--- src/components/home/Search.jsx
+++ src/components/home/Search.jsx
@@ -17,16 +17,15 @@ const Search = ({selectChange, position, onChange, date, onSearch}) => {
         control: (defaultStyles) => ({
             ...defaultStyles,
             backgroundColor: "#dccbc1",
-            borderBottomLeftRadius: '10px',
-            borderTopLeftRadius: '10px',
+            // borderBottomLeftRadius: '10px',
+            // borderTopLeftRadius: '10px',
             borderTopRightRadius: '0px',
             borderTopBottomRadius: '0px',
-            width: '300px',
+            // width: '300px',
             padding: "10px",
             border: "none",
             boxShadow: "none",
             cursor: 'pointer',
-
         }),
         singleValue: (defaultStyles) => ({ ...defaultStyles, color: "#fff" }),
     };
@@ -37,23 +36,25 @@ const Search = ({selectChange, position, onChange, date, onSearch}) => {
 
     const [startDate, endDate] = date;
     return(
-        <section className={'w-full tablet:h-[500px] bg-secondary shadow-md font-primary text-default tablet:p-20'}>
-            <div className={'tablet:flex tablet:flex-col gap-[20px] text-white justify-center items-center'}>
-                <h1 className={'text-title'}>Поиск отелей</h1>
+        <section className={"w-full h-[300px] bg-hero-pattern bg-no-repeat bg-cover bg-center bg-fixed  tablet:shadow-md font-primary text-default tablet:p-20 px-[15px]"}>
+            <div className={'flex flex-col gap-[20px] text-white justify-center items-center'}>
+                <h1 className={'text-title text-black tablet:text-white'}>Поиск отелей</h1>
                     <Formik>
                         <Form
-                            className={'flex bg-white w-[60%] p-[10px] rounded-[15px] gap-2 text-white'}
+                            className={'flex flex-col tablet:flex-row bg-white tablet:w-[60%] w-full p-[10px] rounded-[15px] gap-2 text-white'}
                             onSubmit={(e) => onSearch(e)}
                         >
-                            <Select
-                                placeholder={'Направление'}
-                                options={options}
-                                value={position}
-                                styles={customStyles}
-                                onChange={(target) => selectChange(target)}
-                            />
+                            <div>
+                                <Select
+                                    placeholder={'Направление'}
+                                    options={options}
+                                    value={position}
+                                    styles={customStyles}
+                                    onChange={(target) => selectChange(target)}
+                                />
+                            </div>
                             <DatePicker
-                                className={'block text-white bg-secondary outline-0  p-[15px] cursor-pointer min-w-[300px]'}
+                                className={'block text-white bg-secondary outline-0 p-[15px] cursor-pointer tablet:min-w-[300px] w-full'}
                                 selectsRange={true}
                                 minDate={new Date()}
                                 startDate={startDate}
@@ -64,7 +65,7 @@ const Search = ({selectChange, position, onChange, date, onSearch}) => {
                                 isClearable={true}
                                 placeholderText="Выбор даты"
                             />
-                            <Button value={'Найти'} />
+                            <Button pxSize={'px-[10px] py-[5px]'} value={'Найти'} />
                         </Form>
                     </Formik>
             </div>
